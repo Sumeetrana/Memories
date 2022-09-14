328ad1c895de2b31bf812129cef33082447e1820
sumeet.rana <sumeetsinh28@gmail.com>
2022-09-15 03:02:50 +0530

    feat(memories): routes setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
+import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 const app = express();
+app.use("/posts", postRoutes);
 
 // middlewares
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+// Routes
 
 // Database connection
 const CONNECTION_URL = 'mongodb+srv://Potterhead:Potterhead123@cluster0.m9siizk.mongodb.net/?retryWrites=true&w=majority';
