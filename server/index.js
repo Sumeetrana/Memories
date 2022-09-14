607df9867526f6d88dda2e65dbe88c534805b5ee
sumeet.rana <sumeetsinh28@gmail.com>
2022-09-15 02:56:36 +0530

    chore(memories): database connected to mongodb cloud

--- server/index.js
+++ server/index.js
@@ -4,6 +4,19 @@ import cors from 'cors';
 
 const app = express();
 
+// middlewares
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+// Database connection
+const CONNECTION_URL = 'mongodb+srv://Potterhead:Potterhead123@cluster0.m9siizk.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL).then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running...");
+  })
+}).catch((error) => {
+  console.log(error.message);
+})
