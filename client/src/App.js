cd35d3b23276168690975bade6274693db974601
sumeet.rana <sumeetsinh28@gmail.com>
2022-09-18 02:07:08 +0530

    feat(memories): added basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import memories from './images/memories.png';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align="center">Memories</Typography>
+        <img src={memories} alt="memories" height={60} />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
