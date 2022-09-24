8a2b58e9901e40a5403b922ae807923ef54c891e
sumeet.rana <sumeetsinh28@gmail.com>
2022-09-24 14:35:24 +0530

    feat(memories): redux integrated

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './App';
+import reducers from './reducers';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(<App />, document.getElementById("root"));
